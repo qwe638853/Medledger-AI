3a67ef88c5b5c43c44a2a91d9d4cb96b4f85bf17
qwe638853 <qwe638853@gmail.com>
2025-05-06 02:55:37 +0800

    frontend to hyperledger

--- hyperledger/sdk_test/database/db.go
+++ hyperledger/sdk_test/database/db.go
@@ -14,36 +14,37 @@ func InitDB(path string) error {
 	var err error
 	DB, err = sql.Open("sqlite3", path)
 	if err != nil {
-		return fmt.Errorf("開啟資料庫失敗: %v", err)
+		return fmt.Errorf("無法開啟 SQLite 資料庫: %v", err)
 	}
 
-	// 建立表格
-	sqlStmt := `
+	createStmt := `
 	CREATE TABLE IF NOT EXISTS users (
-		id TEXT PRIMARY KEY,
+		username TEXT PRIMARY KEY,
+		password TEXT,
 		name TEXT,
+		date TEXT,
 		email TEXT,
-		password TEXT
-	);
-	`
-	_, err = DB.Exec(sqlStmt)
+		phone TEXT
+	);`
+	_, err = DB.Exec(createStmt)
 	if err != nil {
-		return fmt.Errorf("建立表格失敗: %v", err)
+		return fmt.Errorf("建立資料表失敗: %v", err)
 	}
 
 	log.Println("✅ SQLite 初始化成功")
 	return nil
 }
 
-func InsertUser(id, name, email, password string) error {
-	_, err := DB.Exec("INSERT INTO users(id, name, email, password) VALUES (?, ?, ?, ?)", id, name, email, password)
-	return err
+// 查詢帳號是否存在
+func IsUserExists(username string) (bool, error) {
+	var count int
+	err := DB.QueryRow("SELECT COUNT(*) FROM users WHERE username = ?", username).Scan(&count)
+	return count > 0, err
 }
 
-func GetUserByID(id string) (string, string, string, error) {
-	row := DB.QueryRow("SELECT name, email, password FROM users WHERE id = ?", id)
-	
-	var name, email, password string
-	err := row.Scan(&name, &email, &password)
-	return name, email, password, err
+// 新增使用者
+func InsertUser(username, password, name, date, email, phone string) error {
+	_, err := DB.Exec("INSERT INTO users(username, password, name, date, email, phone) VALUES (?, ?, ?, ?, ?, ?)",
+		username, password, name, date, email, phone)
+	return err
 }
--- hyperledger/sdk_test/main.go
+++ hyperledger/sdk_test/main.go
@@ -8,8 +8,10 @@ import (
 	"net"
 	"net/http"
 
+	db "sdk_test/database"
 	fc "sdk_test/fabric"
-	pb "sdk_test/proto/myhealth"
+	pb "sdk_test/proto"
+	sc "sdk_test/service"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/hyperledger/fabric-gateway/pkg/client"
@@ -60,70 +62,71 @@ func (s *server) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResp
 
 // Register
 func (s *server) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
-	log.Printf("Received Register: %v", req)
-	return &pb.RegisterResponse{
-		Success: true,
-		Message: "Register successful",
-	}, nil
+	return sc.HandleRegister(ctx, req)
 }
 
-
 func main() {
-	userID := "User20000"
-	// 1. 產生私鑰 & CSR
-	privKey, csrPEM, err := fc.GenerateCSR(userID)
+	err := db.InitDB("database/user_data.sqlite")
 	if err != nil {
-		panic(err)
+		log.Fatalf("❌ SQLite 初始化失敗: %v", err)
 	}
-	log.Println("✅ 私鑰 & CSR 產生成功", privKey, csrPEM)
-	
-	err = fc.SaveCSRToFile(csrPEM,"csr.pem")
-	if err != nil {
-		log.Fatalf("❌ 寫入CSR失敗: %v", err)
-	}
-	log.Println("📁 寫入CSR成功")
-
-	err = fc.SavePrivateKeyToFile(privKey,"server.key")
-	if err != nil {
-		log.Fatalf("❌ 寫入私鑰失敗: %v", err)
-	}
-	log.Println("📁 寫入私鑰成功")
-
-
-	err = fc.RegisterUser("http://localhost:7054", "../orgs/org1.example.com/users/org1-admin/msp/signcerts/cert.pem", "../orgs/org1.example.com/users/org1-admin/msp/keystore/server.key", fc.RegisterRequest{
-		ID:          userID,
-		Secret:      "pw123",
-		Affiliation: "org1.department1",
-		Type:        "client",
-	})
-	if err != nil {
-		log.Fatalf("❌ 註冊失敗: %v", err)
-	}
-	log.Println("✅ 用戶註冊成功")
-
-	err = fc.EnrollUser("http://localhost:7054",userID, "pw123", fc.EnrollRequest{
-		Certificate_request: string(csrPEM),
-		Profile: "",
-	})
-	if err != nil {
-		log.Fatalf("❌ 登入失敗: %v", err)
-	}
-	log.Println("✅ 用戶登入成功")
-	
 
 	/*
-	cert, key, err := fc.EnrollUser("http://localhost:7054", fc.EnrollRequest{
-		Username: "User877",
-		Password: "pw123",
-	})
-	if err != nil {
-		log.Fatalf("❌ Enroll 失敗: %v", err)
-	}
-	log.Printf("✅ Enroll 成功:\nCert:\n%s\nKey:\n%s", cert, key)
-*/
+		userID := "User20000"
+		// 1. 產生私鑰 & CSR
+		privKey, csrPEM, err := fc.GenerateCSR(userID)
+		if err != nil {
+			panic(err)
+		}
+		log.Println("✅ 私鑰 & CSR 產生成功", privKey, csrPEM)
+
+		err = fc.SaveCSRToFile(csrPEM,"csr.pem")
+		if err != nil {
+			log.Fatalf("❌ 寫入CSR失敗: %v", err)
+		}
+		log.Println("📁 寫入CSR成功")
+
+		err = fc.SavePrivateKeyToFile(privKey,"server.key")
+		if err != nil {
+			log.Fatalf("❌ 寫入私鑰失敗: %v", err)
+		}
+		log.Println("📁 寫入私鑰成功")
+
+
+		err = fc.RegisterUser("http://localhost:7054", "../orgs/org1.example.com/users/org1-admin/msp/signcerts/cert.pem", "../orgs/org1.example.com/users/org1-admin/msp/keystore/server.key", fc.RegisterRequest{
+			ID:          userID,
+			Secret:      "pw123",
+			Affiliation: "org1.department1",
+			Type:        "client",
+		})
+		if err != nil {
+			log.Fatalf("❌ 註冊失敗: %v", err)
+		}
+		log.Println("✅ 用戶註冊成功")
+
+		err = fc.EnrollUser("http://localhost:7054",userID, "pw123", fc.EnrollRequest{
+			Certificate_request: string(csrPEM),
+			Profile: "",
+		})
+		if err != nil {
+			log.Fatalf("❌ 登入失敗: %v", err)
+		}
+		log.Println("✅ 用戶登入成功")
+
+
+
+		cert, key, err := fc.EnrollUser("http://localhost:7054", fc.EnrollRequest{
+			Username: "User877",
+			Password: "pw123",
+		})
+		if err != nil {
+			log.Fatalf("❌ Enroll 失敗: %v", err)
+		}
+		log.Printf("✅ Enroll 成功:\nCert:\n%s\nKey:\n%s", cert, key)
+	*/
 	fabric := fc.NewFabricContract()
 	defer fabric.Gateway.Close()
-	
+
 	go startGrpcServer()     // 開 gRPC server
 	startHttpGatewayServer() // 開 gRPC-Gateway server (HTTP server)
 }
@@ -219,4 +222,3 @@ func testUploadClaimRead(contract *client.Contract) {
 	fmt.Println("📄 Report:")
 	fmt.Println(string(result))
 }
-
